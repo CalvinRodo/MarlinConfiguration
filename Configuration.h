e3c29dd179ff9e64269bc6c82786e87e4d0e61b6
Calvin Rodo <calvinv@gmail.com>
2022-08-21 23:24:55 -0400

    feat: pid autotune

--- Configuration.h
+++ Configuration.h
@@ -668,9 +668,9 @@
     #define DEFAULT_Kd_LIST {  78.81,  78.81 }
   #else
     // Ender 3 v2
-    #define DEFAULT_Kp  28.72
-    #define DEFAULT_Ki   2.62
-    #define DEFAULT_Kd  78.81
+    #define DEFAULT_Kp 21.0749
+    #define DEFAULT_Ki 1.5499   
+    #define DEFAULT_Kd 71.6413
   #endif
 #endif
 
@@ -751,9 +751,9 @@
   //#define PID_BED_DEBUG // Print Bed PID debug data to the serial port.
 
   // Ender 3 V2
-  #define DEFAULT_bedKp 462.10
-  #define DEFAULT_bedKi  85.47
-  #define DEFAULT_bedKd 624.59
+  #define DEFAULT_bedKp 207.5046
+  #define DEFAULT_bedKi 39.9047
+  #define DEFAULT_bedKd 719.3495
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
